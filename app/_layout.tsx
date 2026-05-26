da86c08c34d08aaae0d7a08d511b73e70eedca78
jsneham <sneha.jadhav2402@gmail.com>
2026-05-26 13:38:32 +0530

    Splash Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { SplashScreen, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import "@/global.css";
@@ -33,9 +32,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </ThemeProvider>
   );
 }
