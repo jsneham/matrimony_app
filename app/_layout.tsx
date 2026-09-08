b975046ec66ab460e823b5c1ce5742085927b605
jsneham <sneha.jadhav2402@gmail.com>
2026-09-08 20:41:03 +0530

    account fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "expo-router/react-navigation";
 import { SplashScreen, Stack } from "expo-router";
 import "react-native-reanimated";
 
