00f7baf2a72c50f68fbd381a98a1d8c14f5246ec
harshaddpatkar <friendharshad4ever@gmail.com>
2026-09-08 22:30:22 +0530

    text size fixes

--- components/profile/ProfileTabBar.tsx
+++ components/profile/ProfileTabBar.tsx
@@ -2,7 +2,6 @@
 import { Text } from "@/components/ui/Text";
 import { colors } from "@/constants/theme";
 import { TabConfig } from "@/types/profile";
-import { platformTextSize } from "@/utils/platformTextSize";
 import React, { useState } from "react";
 import { TouchableOpacity, View } from "react-native";
 
@@ -37,9 +36,10 @@ export const ProfileTabBar: React.FC<ProfileTabBarProps> = ({
               }}
               numberOfLines={1}
               adjustsFontSizeToFit
-              className={`${platformTextSize(13)} leading-none text-center font-bold ${
-                isActive ? "text-black" : "text-gray"
-              }`}
+              // className={`${platformTextSize(13)} leading-none text-center font-bold ${
+              //   isActive ? "text-black" : "text-gray"
+              // }`}
+              className={`text-13 leading-none text-center font-bold ${isActive ? "text-black" : "text-gray"}`}
             >
               {tab.label}
             </Text>
--- utils/platformTextSize.ts
+++ utils/platformTextSize.ts
@@ -1,6 +1,8 @@
 import { Platform } from "react-native";
 
 export function platformTextSize(iosSize: number): string {
-  const size = Platform.OS === "android" ? iosSize - 1 : iosSize;
+  const size = Platform.OS === "android" ? iosSize - 2 : iosSize;
+  console.log("size", Platform.OS, size);
+  
   return `text-${size}`;
 }
